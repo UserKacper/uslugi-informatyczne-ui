1e12a121ac871d16d14a754fba76018fec027429
Kacper <kdeja.webdev@gmail.com>
2024-07-03 23:01:34 +0200

    main navigation bar, no functionality mobile responsive initial commit + setup tailwind and shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center">Main Content</main>
+  );
+}
---
+++ src/components/main-nav.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useState } from "react"
+import { Button } from "./ui/button"
+import Hamburger from "hamburger-react"
+
+export const NavigationBar = () => {
+    const [isToggled, setIsToggled] = useState<Boolean>(false)
+    return (<>
+        <main className="h-[100px] sm:h-[150px] bg-slate-300">
+            <section className="flex h-full w-full p-5 xl:justify-evenly sm:justify-between">
+                <div className="self-center">
+                    <div>LOGO</div>
+                </div>
+                <div className="self-center hidden sm:flex gap-3">
+                    <Button variant={"ghost"}>O nas</Button>
+                    <Button variant={"ghost"}>Cennik</Button>
+                    <Button variant={"ghost"}>Kontakt</Button>
+                </div>
+                <div className="self-center flex justify-end w-full sm:hidden">
+                    <Hamburger onToggle={setIsToggled} />
+                </div>
+            </section>
+        </main>
+        {isToggled && <div className="flex justify-center flex-col sm:hidden">
+            <Button variant={"ghost"}>Kontakt</Button>
+            <Button variant={"ghost"}>Cennik</Button>
+            <Button variant={"ghost"}>Kontakt</Button>
+            </div>
+        }
+    </>)
+}
