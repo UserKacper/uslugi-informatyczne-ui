ece8895185531a70890145968889047f8ce73483
Kacper Deja <kacper.10744561@ltimindtree.com>
2024-07-12 08:20:56 +0200

    feat: bookmarks selection styling

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -13,14 +13,14 @@ const PricingPage = () => {
         { name: 'Aplikacje Mobilne', }
     ]
 
-    return (
+    return (<>
         <section className='w-full h-[100px]'>
-            <div className='self-center flex justify-evenly h-full'>
+            <div className='self-center flex justify-evenly h-full bg-accent text-accent-foreground'>
                 {plans.map(plan => (
                     <Button
                         variant={"outline"}
                         key={plan.name}
-                        className={`${selectedOption === plan.name ? 'w-full h-full self-center rounded-none border-none -translate-y-5 duration-500 transition-transform rounded-t-md' : 'w-full h-full self-center rounded-none border-none'} `}
+                        className={`${selectedOption === plan.name ? 'w-full h-full self-center rounded-none border-none -translate-y-5 duration-200 transition-transform rounded-t-md bg-accent text-accent-foreground' : 'w-full h-full self-center rounded-none border-none'} `}
                         onClick={() => setSelectedOption(plan.name)}
                     >
                         {plan.name}
@@ -28,7 +28,8 @@ const PricingPage = () => {
                 ))}
             </div>
         </section>
-    )
+        <div className='h-screen bg-purple-400'>where am i</div>
+    </>)
 }
 
 export default PricingPage
