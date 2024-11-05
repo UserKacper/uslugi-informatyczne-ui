23210ddb5b259fde2afa9435f9ef2c2cb0ee1ceb
UserKacper <kdeja.webdev@gmail.com>
2024-11-05 19:18:09 +0100

    feat: defining paths for 404 not foung page

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,36 @@ import { ServiceCard } from "./components/pages/ServiceCards";
 import Contact from "./components/pages/ContactPage";
 import PrivacyPolicy from "./components/pages/PolicyPrivacyPage";
 import { TermsPage } from "./components/pages/TermsPage";
+import ErrorPage from "./components/pages/ErrorPage";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavigationBar />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="terms" element={<TermsPage />} />
-          <Route path="privacy" element={<PrivacyPolicy />} />
-          <Route path="price" element={<PlanSelection />}>
-            <Route path="/price/:plan" element={<ServiceCard />} />
-          </Route>
+
+  const definedPaths =
+    [
+      {
+        path: "/",
+        element: <NavigationBar />
+      },
+      {
+
+      }
+    ]
+}
+
+return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="terms" element={<TermsPage />} />
+        <Route path="privacy" element={<PrivacyPolicy />} />
+        <Route path="price" element={<PlanSelection />}>
+          <Route path="/price/:plan" element={<ServiceCard />} />
         </Route>
-      </Routes >
-    </BrowserRouter >
-  );
+        <Route path="error" element={<ErrorPage />} />
+      </Route>
+    </Routes >
+  </BrowserRouter >
+);
 }
---
+++ src/components/pages/ErrorPage.tsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+    return (
+        <main className="h-screen">
+            <div className="flex justify-center self-center">
+                404 not found - Page does not exist
+            </div>
+        </main>
+    )
+}
+
+export default ErrorPage
