cea823f65449376f54e8b73241541dad91baa607
UserKacper <kdeja.webdev@gmail.com>
2024-11-07 18:24:51 +0100

    feat: ux/ui cards services full responsive

--- src/components/comps/ServiceCards.tsx
+++ src/components/comps/ServiceCards.tsx
@@ -63,17 +63,16 @@ export const ServiceCard = () => {
     }, [selectedPath]);
 
     return (
-        <section className='flex flex-col gap-6 lg:justify-evenly lg:flex-row p-5 h-full'>
+        <section className='flex flex-col gap-6 lg:justify-evenly lg:flex-row p-5'>
             {dataService?.map((data) => (
-                <React.Fragment key={data.id}>  
-                    <div className='self-center w-full max-w-lg h-full p-4 bg-white border border-gray-200 rounded-lg hover:shadow-custom duration-1000 sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400'><h5>{data.fullName}</h5>
+                <React.Fragment key={data.id}>
+                    <div className='flex max-lg:text-center flex-col self-center w-full max-sm:max-w-xs lg:max-w-xs max-w-lg p-4 bg-white border border-gray-200 rounded-lg hover:shadow-custom duration-1000 sm:p-8 dark:bg-gray-800 dark:border-gray-700' style={{ "height": 450 }}><h5>{data.fullName}</h5>
                         <div className="flex items-baseline text-gray-900 dark:text-white gap-6 font-bold">Od
                             <span className="text-3xl font-extrabold tracking-tight">{data?.pricingMin + " "}
                                 <span className="text-2xl font-semibold">{data?.pricingCurrency}</span>
                             </span>
                         </div>
-                        <ul role="list" className="space-y-5 my-7">
+                        <ul role="list" className="space-y-4 max-lg:space-y-6 my-7 h-full text-start">
                             <li className="flex items-center self-center">
                                 <svg className="flex-shrink-0 w-4 h-4 text-[#6F43FC] dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
